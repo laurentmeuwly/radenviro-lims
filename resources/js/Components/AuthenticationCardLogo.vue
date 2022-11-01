1561c71b0cf0dd79f3ec9a4f742138ca019c23f1
Laurent Meuwly <laurent.meuwly@radiofr.ch>
2022-11-01 13:49:35 +0100

    first commit

---
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <Link :href="'/'">
+        <img :src="'/images/logo.png'" style="width: 230px; height: 57px" />
+    </Link>
+    <span>Radenviro</span>
+</template>
